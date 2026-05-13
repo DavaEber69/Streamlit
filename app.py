c07becd35d8f28c78750e4734fb688152e0b18ad
David Davayan <103106738+DavaEber69@users.noreply.github.com>
2026-05-13 10:09:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -16,13 +16,44 @@ model = joblib.load('breast_cancer_model.pkl')
 st.title('Breast Cancer Prediction')
 
 mean_radius = st.number_input('Mean Radius', value=14.0)
-
+input_data = pd.DataFrame({
+    'mean radius': [mean_radius],
+    'mean texture': [20.0],
+    'mean perimeter': [90.0],
+    'mean area': [600.0],
+    'mean smoothness': [0.1],
+    'mean compactness': [0.1],
+    'mean concavity': [0.1],
+    'mean concave points': [0.05],
+    'mean symmetry': [0.2],
+    'mean fractal dimension': [0.06],
+    'radius error': [0.5],
+    'texture error': [1.0],
+    'perimeter error': [3.0],
+    'area error': [40.0],
+    'smoothness error': [0.005],
+    'compactness error': [0.02],
+    'concavity error': [0.03],
+    'concave points error': [0.01],
+    'symmetry error': [0.02],
+    'fractal dimension error': [0.003],
+    'worst radius': [16.0],
+    'worst texture': [25.0],
+    'worst perimeter': [110.0],
+    'worst area': [800.0],
+    'worst smoothness': [0.15],
+    'worst compactness': [0.25],
+    'worst concavity': [0.3],
+    'worst concave points': [0.12],
+    'worst symmetry': [0.3],
+    'worst fractal dimension': [0.08]
+})
 if st.button('Predict'):
 
     data = pd.DataFrame({
         'mean radius': [mean_radius]
     })
 
-    prediction = model.predict(data)
+    prediction = model.predict(input_data)
 
     st.write(prediction)
